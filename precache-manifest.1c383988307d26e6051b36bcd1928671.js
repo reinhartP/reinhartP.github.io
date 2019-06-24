4c389cb0392392c3852d83bc45a007920cb24473
reinhartP <reinhartjp@gmail.com>
2019-06-23 17:16:26 -0700

    updates

---
+++ precache-manifest.1c383988307d26e6051b36bcd1928671.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "77ef082ebd08648333ad",
+    "url": "/static/js/main.77ef082e.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "77ef082ebd08648333ad",
+    "url": "/static/css/main.11297dda.chunk.css"
+  },
+  {
+    "revision": "d761015c5faf5fbc2783c1844531cf65",
+    "url": "/index.html"
+  }
+];
